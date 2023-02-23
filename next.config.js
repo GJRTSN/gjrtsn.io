9913f4461a10118ca7d6c8bc37bf8a0303a49234
GJRTSN <98411330+GJRTSN@users.noreply.github.com>
2023-02-24 00:51:08 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
+  },
+};
