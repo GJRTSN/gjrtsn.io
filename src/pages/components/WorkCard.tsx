8b0aa7e22e40ca341f93c6c9b55217c32db2a601
GJRTSN <98411330+GJRTSN@users.noreply.github.com>
2023-03-01 09:57:43 +0100

    Update WorkCard.tsx
    Fixed border-radius on image

--- src/pages/components/WorkCard.tsx
+++ src/pages/components/WorkCard.tsx
@@ -15,14 +15,14 @@ const WorkCard: FC<WorkCardProps> = ({
   return (
     <div
       id="maincard"
-      className={`group flex flex-col md:flex-row bg-gray-200 rounded-md w-full max-w-6xl my-4 transition-all duration-500 hover:shadow-2xl hover:shadow-white/20 hover:bg-white relative ${
+      className={`group flex flex-col md:flex-row bg-gray-200 rounded-lg w-full max-w-6xl my-4 transition-all duration-500 hover:shadow-2xl hover:shadow-white/20 hover:bg-white relative ${
         flip ? "md:flex-row-reverse" : "md:flex-row"
       }`}
     >
-      <div className="relative h-full w-full md:w-1/2 mb-8 md:mb-0 mobile:hidden">
-        <div className="relative h-full">
+      <div className="relative h-full w-full md:w-1/2 mb-8  md:mb-0 mobile:hidden">
+        <div className="relative h-full rounded-lg">
           <Image
-            className="w-full h-full object-cover object-center"
+            className="w-full h-full object-cover object-center rounded-lg"
             src={thumbnail}
             alt={title}
             layout="fill"
@@ -30,18 +30,20 @@ const WorkCard: FC<WorkCardProps> = ({
           />
           <div
             id="shade"
-            className="absolute inset-0 bg-black opacity-30 transition-opacity duration-500 group-hover:opacity-0"
+            className={`absolute  inset-0 bg-black opacity-30 transition-opacity duration-500 group-hover:opacity-0"${
+              flip ? "rounded-r-lg" : "rounded-l-lg"
+            }`}
           ></div>
         </div>
       </div>
 
       <div
-        className={`flex flex-col justify-center w-full ${
+        className={`flex flex-col justify-center w-full  ${
           flip ? "md:pl-8" : "md:pr-8"
         } md:w-1/2 px-4 p-8 md:order-2 ${flip ? "md:order-1" : "md:order-2"}`}
       >
         <Link href={`/work/${slug}`}>
-          <h2 className="inline-block z-30 text-2xl text-black font-bold my-2 hover:text-rose-500 transition-all  duration-300 ease-in-out mobile:text-center">
+          <h2 className="inline-block z-30 text-2xl text-black font-bold my-2  hover:text-rose-500 transition-all  duration-300 ease-in-out mobile:text-center">
             {title}
           </h2>
         </Link>
